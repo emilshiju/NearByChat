3f85de60069340169725bbd11ce47f2befcab13c
emil shiju <emilshiju10@gmail.com>
2025-07-05 00:33:02 +0530

    fixed all bugs

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 import { createContext ,useState} from 'react'
-
-export const socket = io("http://localhost:5000");
+// http://localhost:5000
+export const socket = io("https://siof.site");
+// baseURL:'https://siof.site'
 
 export const SocketContext=createContext()
